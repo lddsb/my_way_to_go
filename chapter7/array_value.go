9d1324a6f83a5301ada6199a97f4a3a580bc47a6
luodi <luodi0128@gmail.com>
2017-09-30 18:32:38 +0800

    add .gitignore file to root path.

--- chapter7/array_value.go
+++ chapter7/array_value.go
@@ -6,7 +6,17 @@ import "fmt"
 func main() {
 	arr1 := [10]int{}
 	for i := 0; i < 10; i++ {
-		arr1[i] = i * i;
+		arr1[i] = i * 2;
+	}
+	//  print arr1
+	fmt.Println("This is for:")
+	for i := 0; i < len(arr1); i++ {
+		fmt.Printf("Array at index %d is %d\n", i, arr1[i])
+	}
+	//  for range
+	fmt.Println("This is for range:")
+	for i, val := range arr1 {
+		fmt.Printf("Array index %d is %d\n", i, val)
 	}
 	arr2 := arr1
 	arr2[1] = 10
